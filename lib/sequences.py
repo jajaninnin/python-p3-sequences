94497bf1190b9461fa637640cf63f3fa826ac060
janine pamintuan <jaja.ninnin@gmail.com>
2024-11-18 19:56:39 -0500

    fibonacci

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if (length == 0):
+        print([])
+        return []
+    if (length == 1): 
+        print([0])
+        return [0]
+    resultArr = [0, 1]
+    for n in range(2, length): 
+        resultArr.append(resultArr[n - 2] + resultArr[n - 1])
+    print(resultArr)
+    return resultArr
